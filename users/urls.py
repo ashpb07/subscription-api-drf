ce96d7f7b9cb4884ad6293eac445626cc986e928
Anish G Prabhu <prabhuanish6@gmail.com>
2025-10-25 12:24:31 +0530

    added models and did some jwt auth config

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from django.urls import path
 from .views import *
+
 urlpatterns = [
-    path('',index)
-
-
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
