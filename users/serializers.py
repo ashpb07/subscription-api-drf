aeffd17a1cf1be7ffbbf01a73ccf7af45f38329e
Anish G Prabhu <prabhuanish6@gmail.com>
2025-10-28 14:26:16 +0530

    added the following
    >register user with in built django user auth
    
     uses jwt + with the user model
    
    login setup also added

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,31 @@
 from rest_framework import serializers
-from models import Userinfo
+from .models import Userinfo
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
-class UserSerializer(serializers.Serializer):
-    pass
+
+
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+    
+    # def validate_password(self,value):
+    #     if self.pass1 != self.pass2 :
+    #      return Response("User password does not match", status=status.HTTP_400_BAD_REQUEST)`
+
+    
--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.urls import path
-from .views import *
+from .views import RegisterView
 
 urlpatterns = [
-    
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view() ,name="register"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
